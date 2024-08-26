90559712bd72b13ed652c3813c9d913ccdcb11e7
Francisca Ilabaca <fr23ip@gmail.com>
2024-08-26 17:36:19 +0200

    styling of headerview and headerviewgeneral, modif CustomButton

---
+++ src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/CustomButton.java
@@ -0,0 +1,79 @@
+package fr.isika.cda27.projet1.Annuaire_Isika.view;
+
+import javafx.scene.control.Button;
+
+public class CustomButton extends Button {
+
+	String btnLabel;
+
+	public CustomButton(String btnLabel) {
+		super(btnLabel);
+		this.btnLabel = btnLabel;
+	}
+
+	public CustomButton generateDocumentation() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton adminAccess() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton adminAccessTitle() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton goToDirectory() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton goToDirectoryTitle() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton printDirectory() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton homeButtonTitle() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+	}
+
+	public CustomButton logOut() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton modifyStudent() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton deleteStudent() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+	public CustomButton addStudent() {
+		CustomButton btn = new CustomButton(btnLabel);
+		return btn;
+
+	}
+
+}
--- src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/HeaderView.java
+++ src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/HeaderView.java
@@ -10,31 +10,45 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class HeaderView extends HBox  {
-	
+public class HeaderView extends HBox {
+
 	HBox headerBox = new HBox();
-	
+
 	VBox stripesBoxL = new VBox();
 	Rectangle blueStripeL = new Rectangle(465,12);
 	Rectangle whiteStripeL = new Rectangle(465,12);
 	Rectangle orangeStripeL = new Rectangle(465,12);
-	
+
 	VBox stripesBoxR = new VBox();
 	Rectangle blueStripeR = new Rectangle(465,12);
 	Rectangle whiteStripeR = new Rectangle(465,12);
 	Rectangle orangeStripeR = new Rectangle(465,12);
 
 	String imgPathLogo = "/Images/isikalogo.png";
-	
+
 	Label label;
 
 	public HeaderView(Scene scene) {
 		
+//	double stripeHeightRatio = 0.015;
+		
 	blueStripeL.setFill(Color.web("#144d65"));
 	whiteStripeL.setFill(Color.web("#FFFFFF"));
 	orangeStripeL.setFill(Color.web("#db754a"));
+	
+//	blueStripeL.widthProperty().bind(scene.widthProperty());
+//	blueStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio)); 
+//	
+//	whiteStripeL.widthProperty().bind(scene.widthProperty());
+//	whiteStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio)); 
+//	
+//	orangeStripeL.widthProperty().bind(scene.widthProperty());
+//	orangeStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio)); 
+	
+	
 	stripesBoxL.getChildren().addAll(blueStripeL,whiteStripeL,orangeStripeL);
 	stripesBoxL.setAlignment(Pos.CENTER);
+
 	
 	ImageView imgViewLogo = new ImageView(new Image(getClass().getResourceAsStream(imgPathLogo)));
 	imgViewLogo.setFitWidth(150);
@@ -43,17 +57,25 @@ public class HeaderView extends HBox  {
 	blueStripeR.setFill(Color.web("#144d65"));
 	whiteStripeR.setFill(Color.web("#FFFFFF"));
 	orangeStripeR.setFill(Color.web("#db754a"));
+	
+	
+//	blueStripeR.widthProperty().bind(scene.widthProperty());
+//	blueStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));  
+//	
+//	whiteStripeR.widthProperty().bind(scene.widthProperty());
+//	whiteStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));  
+//	
+//	orangeStripeR.widthProperty().bind(scene.widthProperty());
+//	orangeStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));  
+	
+	
 	stripesBoxR.getChildren().addAll(blueStripeR,whiteStripeR,orangeStripeR);
 	stripesBoxR.setAlignment(Pos.CENTER);
+	
 	
 	headerBox.getChildren().addAll(stripesBoxL, imgViewLogo, stripesBoxR);
 	this.getChildren().addAll(headerBox);
 
 	}
-	
-
-
-
-	
 
 }
