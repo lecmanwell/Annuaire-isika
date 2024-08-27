75b9684375b7b81291a525b14d5dab8dacbb0b57
Francisca Ilabaca <fr23ip@gmail.com>
2024-08-27 08:54:22 +0200

    custombutton clean

--- src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/CustomButton.java
+++ src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/CustomButton.java
@@ -1,18 +1,5 @@
 package fr.isika.cda27.projet1.Annuaire_Isika.view;
 
-<<<<<<< HEAD
-public class Button {
-
-	String btnLabel;
-
-	public Button(String btnLabel) {
-		super();
-		this.btnLabel = btnLabel;
-	}
-
-	public Button generateDocumentation() {
-		Button btn = new Button(btnLabel);
-=======
 import javafx.scene.control.Button;
 
 public class CustomButton extends Button {
@@ -26,70 +13,40 @@ public class CustomButton extends Button {
 
 	public CustomButton generateDocumentation() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button adminAccess() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton adminAccess() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button adminAccessTitle() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton adminAccessTitle() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button goToDirectory() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton goToDirectory() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button goToDirectoryTitle() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton goToDirectoryTitle() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button printDirectory() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton printDirectory() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button homeButtonTitle() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton homeButtonTitle() {
 		CustomButton btn = new CustomButton(btnLabel);
 		return btn;
@@ -97,46 +54,24 @@ public class CustomButton extends Button {
 
 	public CustomButton logOut() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button logOut() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton modifyStudent() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button modifyStudent() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton deleteStudent() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
 
-<<<<<<< HEAD
-	public Button deleteStudent() {
-		Button btn = new Button(btnLabel);
-		return btn;
-
-	}
-
-	public Button addStudent() {
-		Button btn = new Button(btnLabel);
-=======
 	public CustomButton addStudent() {
 		CustomButton btn = new CustomButton(btnLabel);
->>>>>>> 90559712bd72b13ed652c3813c9d913ccdcb11e7
 		return btn;
 
 	}
