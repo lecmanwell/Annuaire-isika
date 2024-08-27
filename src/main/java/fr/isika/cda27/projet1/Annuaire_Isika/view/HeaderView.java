5e3ef8989c5dc6fe390028709198634b6f077b9e
Francisca Ilabaca <fr23ip@gmail.com>
2024-08-27 08:45:39 +0200

    HeaderView pour Home fini

--- src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/HeaderView.java
+++ src/main/java/fr/isika/cda27/projet1/Annuaire_Isika/view/HeaderView.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,66 +16,69 @@ public class HeaderView extends HBox {
 	HBox headerBox = new HBox();
 
 	VBox stripesBoxL = new VBox();
-	Rectangle blueStripeL = new Rectangle(465,12);
-	Rectangle whiteStripeL = new Rectangle(465,12);
-	Rectangle orangeStripeL = new Rectangle(465,12);
+	Rectangle blueStripeL = new Rectangle();
+	Rectangle whiteStripeL = new Rectangle();
+	Rectangle orangeStripeL = new Rectangle();
 
 	VBox stripesBoxR = new VBox();
-	Rectangle blueStripeR = new Rectangle(465,12);
-	Rectangle whiteStripeR = new Rectangle(465,12);
-	Rectangle orangeStripeR = new Rectangle(465,12);
+	Rectangle blueStripeR = new Rectangle();
+	Rectangle whiteStripeR = new Rectangle();
+	Rectangle orangeStripeR = new Rectangle();
 
 	String imgPathLogo = "/Images/isikalogo.png";
 
 	Label label;
 
 	public HeaderView(Scene scene) {
-		
-//	double stripeHeightRatio = 0.015;
-		
-	blueStripeL.setFill(Color.web("#144d65"));
-	whiteStripeL.setFill(Color.web("#FFFFFF"));
-	orangeStripeL.setFill(Color.web("#db754a"));
-	
-//	blueStripeL.widthProperty().bind(scene.widthProperty());
-//	blueStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio)); 
-//	
-//	whiteStripeL.widthProperty().bind(scene.widthProperty());
-//	whiteStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio)); 
-//	
-//	orangeStripeL.widthProperty().bind(scene.widthProperty());
-//	orangeStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio)); 
-	
-	
-	stripesBoxL.getChildren().addAll(blueStripeL,whiteStripeL,orangeStripeL);
-	stripesBoxL.setAlignment(Pos.CENTER);
 
-	
-	ImageView imgViewLogo = new ImageView(new Image(getClass().getResourceAsStream(imgPathLogo)));
-	imgViewLogo.setFitWidth(150);
-	imgViewLogo.setPreserveRatio(true);
-	
-	blueStripeR.setFill(Color.web("#144d65"));
-	whiteStripeR.setFill(Color.web("#FFFFFF"));
-	orangeStripeR.setFill(Color.web("#db754a"));
-	
-	
-//	blueStripeR.widthProperty().bind(scene.widthProperty());
-//	blueStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));  
-//	
-//	whiteStripeR.widthProperty().bind(scene.widthProperty());
-//	whiteStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));  
-//	
-//	orangeStripeR.widthProperty().bind(scene.widthProperty());
-//	orangeStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));  
-	
-	
-	stripesBoxR.getChildren().addAll(blueStripeR,whiteStripeR,orangeStripeR);
-	stripesBoxR.setAlignment(Pos.CENTER);
-	
-	
-	headerBox.getChildren().addAll(stripesBoxL, imgViewLogo, stripesBoxR);
-	this.getChildren().addAll(headerBox);
+		// ratios for the components of the header
+		double stripeHeightRatio = 0.015;
+		double stripesBoxWidthRatio = 0.45;
+		double logoWidthRatio = 0.1;
+
+		// stripes for the left side
+		blueStripeL.setFill(Color.web("#144d65"));
+		whiteStripeL.setFill(Color.web("#FFFFFF"));
+		orangeStripeL.setFill(Color.web("#db754a"));
+
+		blueStripeL.widthProperty().bind(scene.widthProperty().multiply(stripesBoxWidthRatio));
+		blueStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));
+
+		whiteStripeL.widthProperty().bind(scene.widthProperty().multiply(stripesBoxWidthRatio));
+		whiteStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));
+
+		orangeStripeL.widthProperty().bind(scene.widthProperty().multiply(stripesBoxWidthRatio));
+		orangeStripeL.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));
+
+		stripesBoxL.getChildren().addAll(blueStripeL, whiteStripeL, orangeStripeL);
+		stripesBoxL.setAlignment(Pos.CENTER);
+
+		// logo
+		ImageView imgViewLogo = new ImageView(new Image(getClass().getResourceAsStream(imgPathLogo)));
+		imgViewLogo.fitWidthProperty().bind(scene.widthProperty().multiply(logoWidthRatio));
+		imgViewLogo.setPreserveRatio(true);
+
+		// stripes for the right side
+		blueStripeR.setFill(Color.web("#144d65"));
+		whiteStripeR.setFill(Color.web("#FFFFFF"));
+		orangeStripeR.setFill(Color.web("#db754a"));
+
+		blueStripeR.widthProperty().bind(scene.widthProperty().multiply(stripesBoxWidthRatio));
+		blueStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));
+
+		whiteStripeR.widthProperty().bind(scene.widthProperty().multiply(stripesBoxWidthRatio));
+		whiteStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));
+
+		orangeStripeR.widthProperty().bind(scene.widthProperty().multiply(stripesBoxWidthRatio));
+		orangeStripeR.heightProperty().bind(scene.heightProperty().multiply(stripeHeightRatio));
+
+		stripesBoxR.getChildren().addAll(blueStripeR, whiteStripeR, orangeStripeR);
+		stripesBoxR.setAlignment(Pos.CENTER);
+
+		// add components to the header
+		headerBox.getChildren().addAll(stripesBoxL, imgViewLogo, stripesBoxR);
+		headerBox.setAlignment(Pos.CENTER);
+		this.getChildren().addAll(headerBox);
 
 	}
 
